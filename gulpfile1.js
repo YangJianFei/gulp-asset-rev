165cb243393b86d307209145d9840f0c86bce0cb
Fly-Yang <1294485765@qq.com>
2016-09-02 17:01:21 +0800

    Create gulpfile1.js

---
+++ gulpfile1.js
@@ -0,0 +1,15 @@
+var gulp = require('gulp');
+var assetRev = require('gulp-asset-rev');
+ 
+gulp.task('rev',['revCss'],function() {
+    gulp.src("./test/test.html")
+        .pipe(assetRev())
+        .pipe(gulp.dest('./dest'));
+});
+ 
+gulp.task('revCss',function () {
+    return gulp.src('./test/styles/test.css')
+        .pipe(assetRev())
+        .pipe(gulp.dest('./dest/styles/'))
+});
+gulp.task('default',['rev']);
